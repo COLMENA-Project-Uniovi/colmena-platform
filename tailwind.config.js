a69ac8255896c720d14c60e2a9e52e36b88aeb41
Elmerjcs <elmercortezsanjinez@gmail.com>
2022-10-28 13:39:13 +0200

    Seleccion de proyecto tras login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
 module.exports = {
   darkMode: 'class', // or 'media' or 'class'
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  content: [
-    "./node_modules/flowbite/**/*.js"
-  ],
   theme: {
     flex: {
       '1/2': '1 1 50%',
       'full': '1 1 100%',
     },
     extend: {
-      colors: {
-        'bg-dark-mode': '#303030',
-      },
+
     }
   }
 }
