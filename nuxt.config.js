cfafb45e7e3e6ebe93f32dda2446280a61862007
Elmer José Cortez Sanjinez <UO257192@uniovi.es>
2022-12-29 11:41:13 +0100

    Test deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,6 +74,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [
+      'defu',
+    ],
     postcss: {
       plugins: {
         tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class=" m-auto relative">
-    <div class=" justify-center h-screen pt-24 relative overflow-hidden">
+  <section class="relative m-auto ">
+    <div class="relative justify-center h-screen pt-24 overflow-hidden ">
       <div class="blob1">
         <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
           <path fill="#F59E0B"
@@ -15,11 +15,11 @@
             transform="translate(100 100)" />
         </svg>
       </div>
-      <div class="container flex flex-wrap m-auto h-full z-10 lg:p-10">
-        <div class="basis-1/2 grow flex items-start justify-center flex-col gap-8 z-10">
-          <h1 class="text-8xl font-bold">Conoce tus <br> errores de <br><span class="text-amber-500">programación</span>
+      <div class="container z-10 flex flex-wrap h-full m-auto lg:p-10">
+        <div class="z-10 flex flex-col items-start justify-center gap-8 basis-1/2 grow">
+          <h1 class="font-bold text-8xl">FEo tus <br> errores de <br><span class="text-amber-500">programación</span>
           </h1>
-          <p class="black text-lg leading-7">
+          <p class="text-lg leading-7 black">
             Colmena es una herramienta creada para monitorizar el rendimiento de los alumnos
             durante las sesiones de prácticas en asignaturas de programación. Donde los
             usuarios tienen a su disposición información y métricas sobre el proceso de
@@ -27,7 +27,7 @@
           </p>
           <Link text="Únete" type="primary" href="/register" />
         </div>
-        <div class="basis-1/2 grow flex justify-end items-center">
+        <div class="flex items-center justify-end basis-1/2 grow">
           <img src="~/assets/logos/logo.png" class="w-1/2" alt="Logo" />
         </div>
       </div>
