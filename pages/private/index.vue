4d69a463a8881ee92dacec5f057d2bb93fed5d30
elmer.cortez <35766169+elmercosa@users.noreply.github.com>
2023-04-14 11:58:35 +0200

    private home for student

--- pages/private/index.vue
+++ pages/private/index.vue
@@ -1,39 +1,77 @@
 <template>
-  <div>
-    <Header title="Home" />
-    <section class="section">
-      <div class="flex flex-col w-full p-10 g-5">
-        <h2 class="text-3xl font-bold text-gray-700">Asignaturas</h2>
-        <ul id="example-1" class="flex flex-wrap p-5 mt-5">
-          <li
-            class="w-1/3 h-40 text-xl font-semibold text-black duration-300 bg-white rounded-lg cursor-pointer subject p-9 ease hover:bg-amber-500 hover:text-white"
-            v-for="item in subjects" v-bind:key="item.id" @click="$router.push(`/private/subjects/subject/${item.id}`)">
-            <h2>{{ item.name }}</h2>
-          </li>
-        </ul>
-      </div>
-    </section>
-    
-    <section class="flex p-10 section">
-      <div class="flex flex-col w-6/12 p-5 g-5">
-        <h2 class="text-3xl font-bold text-gray-700">Progreso</h2>
-        <BarChart />
-      </div>
-
-      <!-- <v-calendar></v-calendar> -->
-
-      <div class="flex flex-col w-6/12 p-5 g-5">
-        <h2 class="text-3xl font-bold text-gray-700">Proximas sesiones</h2>
-        <ul id="example-1" class="flex flex-col gap-5 mt-5">
-          <li
-            class="w-full h-40 text-xl font-semibold text-black duration-300 bg-white rounded-lg cursor-pointer subject p-9 ease hover:bg-amber-500 hover:text-white"
-            v-for="item in subjects" v-bind:key="item.id" @click="$router.push(`/private/subjects/subject/${item.id}`)">
-            <h2>{{ item.name }}</h2>
-            <p class="font-thin">{{ item.start_date }}</p>
-          </li>
-        </ul>
-      </div>
-    </section>
+  <div class="grid grid-cols-2 gap-8 px-10 py-8">
+    <div
+      class="relative flex flex-col w-full col-start-1 col-end-3 gap-2 overflow-hidden text-white py-14 px-14 bg-gradient-to-tr from-amber-500 to-amber-300 rounded-xl">
+      <img src="~/assets/logos/logo-white.svg"
+        alt="Logo"
+        class="absolute right-0 h-full scale-105 translate-x-1/2 -translate-y-1/2 opacity-50 top-1/2" />
+      <h2 class="text-3xl font-bold opacity-95">
+        ¡{{ welcomeMessage }}!</h2>
+      <p>Aquí podrás encontrar todo lo que necesitas
+        para conocer tus errores de programación</p>
+    </div>
+    <div
+      class="flex flex-col gap-4">
+      <h2 class="pl-5 text-2xl font-bold">Tus
+        progresos</h2>
+      <BarChart />
+    </div>
+    <div
+    class="flex flex-col gap-4">
+    <h2 class="pl-5 text-2xl font-bold">Tus
+      asignaturas</h2>
+    <ul class="grid grid-cols-2 gap-4">
+      <li v-for="subject in subjects"
+        :key="subject.id">
+        <nuxt-link
+          :to="localePath(`/private/subjects/subject/${subject.id}`)"
+          class="flex flex-col items-center justify-center gap-2 p-4 text-white shadow-lg bg-gradient-to-tr from-colmenablue-600 via-colmenablue-600 to-colmenablue-400 rounded-xl transition-base hover:scale-105">
+          <div
+            class="flex items-end justify-start w-full gap-3">
+            <span
+              class="flex items-center justify-center w-16 h-16 text-xl font-bold rounded-lg bg-colmenablue-400">
+              {{ abbreviate(subject.name) }}
+            </span>
+            <div
+              class="flex flex-col justify-between h-full py-1">
+              <span class="text-gray-200">
+                Semestre {{ subject.semester }}
+              </span>
+              <span
+                class="text-lg font-semibold ">
+                {{ subject.name }}
+              </span>
+            </div>
+          </div>
+          <div
+            class="flex items-baseline justify-end w-full gap-1">
+            <span class="text-sm">
+              por
+            </span>
+            <span class="font-semibold">Carlos
+              Medina</span>
+          </div>
+          <div
+            class="flex items-center justify-center w-full gap-2 border-t-[1px] border-white/30 pt-4 font-semibold text-sm">
+            <span>
+              Sesiones
+            </span>
+            <div class="relative w-full">
+              <div
+                class="absolute left-0 w-full h-3 -translate-y-1/2 bg-white top-50 rounded-xl opacity-50 z-[1]">
+              </div>
+              <div
+                class="absolute left-0 w-[60%] h-3 -translate-y-1/2 bg-white top-50 rounded-tl-xl rounded-bl-xl z-[2]">
+              </div>
+            </div>
+            <span>
+              60%
+            </span>
+          </div>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
   </div>
 </template>
 
@@ -45,13 +83,48 @@ export default {
   },
   auth: true,
   transition: "home",
-  head: {
-    title: "Inicio | Colmena Project",
+  head() {
+    return {
+      title: this.title,
+    }
   },
   data: function () {
     return {
+      title: "Inicio | Colmena Project",
       subjects: 0,
     };
+  },
+  computed: {
+    user() {
+      return this.$auth.$storage.getUniversal("user");
+    },
+    welcomeMessage() {
+      const hora = new Date().getHours();
+      let saludo;
+      if (hora >= 5 && hora < 12) {
+        saludo = `Buenos días ${this.user.name}`;
+      }
+      else if (hora >= 12 && hora < 19) {
+        saludo = `Buenas tardes ${this.user.name}`;
+      }
+      else {
+        saludo = `Buenas noches ${this.user.name}`;
+      }
+      return saludo;
+    }
+  },
+  methods: {
+    abbreviate(text) {
+      const palabras = text
+        .replace("de", "")
+        .replace("la", "")
+        .replace("lo", "")
+        .replace("le", "")
+        .replace("del", "")
+        .split(" ");
+      const iniciales = palabras.map(palabra => palabra.charAt(0));
+      return iniciales.join("");
+    }
   },
   async created() {
     // const data = { id: this.getProject };
@@ -59,6 +132,11 @@ export default {
     const response = await this.$axios.post("academic/subjects/list.json", data);
     const responseJSON = await response;
     this.subjects = responseJSON.data;
+    this.subjects = this.subjects.concat(this.subjects);
+    this.subjects = this.subjects.concat(this.subjects);
   },
+  mounted() {
+    this.$store.commit('SET_PAGE_TITLE', this.title)
+  }
 };
 </script>
