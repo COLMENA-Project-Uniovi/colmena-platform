0944196877f57f25ac0d1003b7753954fd259dc3
elmer.cortez <35766169+elmercosa@users.noreply.github.com>
2023-04-14 11:52:12 +0200

    topbar with page meta

--- components/nav-bars/TopBar.vue
+++ components/nav-bars/TopBar.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="fixed top-0 left-0 flex items-center w-10/12 py-5 pl-24 bg-gray-50 z-[1]">
-        <div class="flex items-center justify-between w-full px-10">
+    <div
+        class="fixed top-0 left-0 flex items-center w-10/12 py-5 pl-24 bg-gray-50 z-[1]">
+        <div
+            class="flex items-center justify-between w-full px-10">
             <div
                 class="flex items-center justify-between text-gray-700">
                 <h2 class="text-xl font-bold">
-                    Inicio
+                    {{ $store.state.pageTitle }}
                 </h2>
             </div>
             <div
