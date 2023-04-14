702b064daa7e426a9fe9f3ef4f587fe9b66047b5
elmer.cortez <35766169+elmercosa@users.noreply.github.com>
2023-04-14 10:27:10 +0200

    topbar with tittle, langs and user

---
+++ components/nav-bars/TopBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="fixed top-0 left-0 flex items-center w-10/12 py-5 pl-24 bg-gray-50 z-[1]">
+        <div class="flex items-center justify-between w-full px-10">
+            <div
+                class="flex items-center justify-between text-gray-700">
+                <h2 class="text-xl font-bold">
+                    Inicio
+                </h2>
+            </div>
+            <div
+                class="flex items-center justify-between gap-2 text-gray-700">
+                <Langs></Langs>
+                <User></User>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
